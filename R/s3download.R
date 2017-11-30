fe861334f24c20954349bbb7614821f48ffa5663
16EAGLE <jxsw@web.de>
2017-11-30 11:23:51 +0100

    include py_path

--- R/s3download.R
+++ R/s3download.R
@@ -1,12 +1,14 @@
 s3download <- function(dir.out, ext, time_range = list("20171101", "20171130"), 
                        instrumentname = "Ocean Land Colour Instrument", producttype = "OL_1_EFR___",
-                       timeliness = "Non Time Critical"){
+                       timeliness = "Non Time Critical", py_path = NULL){
   
   ## Connect to base
   library(RCurl)
   eval(parse(text = RCurl::getURL("https://raw.githubusercontent.com/16EAGLE/16EAGLE/master/R/base.R", .opts = RCurl::curlOptions(followlocation = TRUE))))
   r_load(c("reticulate", "getPass", "raster"))
 
+  if(!is.null(py_path)){use_python(python = py_path)}
+  
   shub.user = "s3guest"
   pass <- "s3guest" #getPass()
   platform <-  "Sentinel-3"
@@ -19,7 +21,7 @@ s3download <- function(dir.out, ext, time_range = list("20171101", "20171130"),
   tmp.file <- file(tmp.gj)
   writeLines(ext.gj, tmp.file)
   close(tmp.file)
-  
+
   reticulate::py_available(initialize = TRUE)
   sat <- py_load("sentinelsat")$sentinelsat
   

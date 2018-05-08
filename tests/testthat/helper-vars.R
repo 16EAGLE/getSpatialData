06572bb8fd6e519cd670a11fcc5d3d153828aefd
16EAGLE <jxsw@web.de>
2018-05-08 15:58:50 +0200

    unit test credentials

--- tests/testthat/helper-vars.R
+++ tests/testthat/helper-vars.R
@@ -7,14 +7,17 @@ vars.sentinel <- list(platform.a = "Sentinel-1",
                       platform.b = "Sentinel-2",
                       platform.c = "Sentinel-3")
 
-vars.auth <- list(dhus.user = "", #getPass::getPass("Enter username: "),
-                  dhus.pass = "", #getPass::getPass("Enter password: "),
+# Sys.setenv("gSD_user" =  getPass::getPass("Enter username: "))
+# Sys.setenv("gSD_pass" =  getPass::getPass("Enter username: "))
+
+vars.auth <- list(dhus.user = Sys.getenv("gSD_user"),
+                  dhus.pass = Sys.getenv("gSD_pass"),
                   s3.user = "s3guest",
                   s3.pass = "s3guest",
-                  ee.user = "",
-                  ee.pass = "",
-                  epsa.user = "",
-                  espa.pass = "")
+                  ee.user = Sys.getenv("gSD_user"),
+                  ee.pass = Sys.getenv("gSD_pass"),
+                  espa.user = Sys.getenv("gSD_user"),
+                  espa.pass = Sys.getenv("gSD_pass"))
 
 if(vars.auth$dhus.user != "") Sys.setenv("gSD_tests_auth" = "yes")
 if(Sys.getenv("gSD_tests_auth") == "yes") gSD_tests_auth <- TRUE else gSD_tests_auth <- FALSE

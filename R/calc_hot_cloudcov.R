05932e01d40a1557fee9fcf8355a387f061185c6
hfisser <henrik.fisser@t-online.de>
2019-04-28 23:56:06 +0200

    documentation

--- R/calc_hot_cloudcov.R
+++ R/calc_hot_cloudcov.R
@@ -10,7 +10,16 @@
 #' 
 #' @note Mainly thought for internal use and only tested for preview images. 
 #' 
-#' @inheritParams calcSentinel_aoi_cloudcov
+#' @param record data.frame, single line representing one record from a records data.frame.
+#' @param preview raster, subject of cloud cover calculation. Either two layers: layer 1 = red, layer 2 = blue. Or three layers: layer 1 = red, layer 2 = something, layer 3 = blue.
+#' @param aoi sp or sf or matrix, the aoi.
+#' @param maxDeviation numeric, the maximum allowed deviation of calculated scene cloud cover from the provided scene cloud cover. Use 100 if you do not like to consider the cloud cover % given by the data distributor. Default is \code{maxDeviation = 20}.
+#' @param sceneCloudCoverCol character, the clear name of the column in the record data.frame where the cloud cover estimation of the data dissiminator is found.
+#' @param cloudPrbThreshold numeric, the threshold of the HOT cloud probability layer (0-100 %) below which pixels are considered as clear sky. Default is \code{cloudPrbThreshold = 40}. It will be dynamically adjusted according to the input in \code{maxDeviation}
+#' @param slopeDefault numeric, value taken as slope ONLY if least-alternate deviation regression fails.  Default is 1.4, proven to work well for common land surfaces.f default values. In this case cloud cover will be set to 9999 % for the given record.
+#' @param interceptDefault, value taken as intercept ONLY if least-alternate deviation regression fails. Default is -10, proven to work well for common land surfaces.
+#' @param dir_out character, optional. Full path to target directory where to save the cloud masks. If \code{NULL}, cloud masks are not saved.
+#' @param verbose logical, if \code{TRUE}, details on the function's progress will be visibile on the console. Default is TRUE.
 #'        
 #' @return A data.frame, one line as the input with one additional column holding the estimated cloud cover within the aoi
 #' 

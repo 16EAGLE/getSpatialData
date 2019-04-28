4a8461bdbda67942a0d86ffc9b1991419cdb945e
hfisser <henrik.fisser@t-online.de>
2019-04-28 17:19:56 +0200

    added option to return preview image to user

--- R/getMODIS_preview.R
+++ R/getMODIS_preview.R
@@ -6,16 +6,23 @@
 #' @param record data.frame, single row data.frame collected from the return of \link{getMODIS_query}, representing the selected record and all its attributes.
 #' @param on_map logical, if \code{TRUE}, the preview is displaed corner-georeferenced on a map. If \code{FALSE}, a simple RGB plot is displayed. Default is \code{TRUE}.
 #' @param show_aoi logical, if \code{TRUE}, the session AOI defined with \link{set_aoi} is drawn to the map viewer. Ignored, if \code{on_map = FALSE} or if no AOI has been defined with \code{set_aoi}. Default is \code{TRUE}.
+#' @param return_preview logical, if \code{TRUE}, the preview image is returned to the user. Default is FALSE.
 #'
-#' @return None. A plot/view display is generated.
+#' @return By default only a plot/view display is generated. If \code{return_preview = TRUE} then the georeferenced preview icon is returned as raster stack.
 #'
 #' @author Jakob Schwalb-Willmann
 #'
 #' @seealso \link{getMODIS_names} \link{getMODIS_query} \link{getMODIS_data}
 #' @export
 
-getMODIS_preview <- function(record, on_map = TRUE, show_aoi = TRUE, verbose = TRUE){
-
-  .EE_preview(record = record, preview_crs = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs", 
-              on_map = on_map, show_aoi = show_aoi, verbose = verbose)
+getMODIS_preview <- function(record, on_map = TRUE, show_aoi = TRUE, return_preview = FALSE, verbose = TRUE){
+  
+  if (isTRUE(return_preview)) {
+    r.prev <- .EE_preview(record = record, preview_crs = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs", 
+                          on_map = on_map, show_aoi = show_aoi, return_preview = return_preview, verbose = verbose)
+    return(r.prev)
+  } else {
+    .EE_preview(record = record, preview_crs = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs", 
+                on_map = on_map, show_aoi = show_aoi, return_preview = return_preview, verbose = verbose)
+  }
 }

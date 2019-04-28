6834b7cabe239a21cd9cdf38b9e71e63e3649d62
hfisser <henrik.fisser@t-online.de>
2019-04-28 22:29:48 +0200

    added inheritsParams from..

--- R/calc_hot_cloudcov.R
+++ R/calc_hot_cloudcov.R
@@ -8,17 +8,8 @@
 #' "An image transform to characterize and compensate for spatial variations in thin cloud contamination of Landsat images", Remote Sensing of Environment 82, 2-3.
 #' HOT seperates clear-sky pixels first from a threshold, calculates a least alternate deviation (LAD) regression from these pixels and exposes cloud pixels by the deviation of all pixels from this clear-sky line.
 #'
-#' @param record data.frame, one line holding a single record from a records data.frame.
-#' @param preview raster, the preview image (RGB) of \code{record}. Should be: layer 1 = red, layer 2 = green, layer 3 = blue. If only two layers are provided, it is assumed: layer 1 = red, layer 2 = blue.
-#' @param aoi sfc_POLYGON or SpatialPolygons or matrix, representing a single multi-point (at least three points) polygon of your area-of-interest (AOI). If it is a matrix, it has to have two columns (longitude and latitude) and at least three rows (each row representing one corner coordinate). If its projection is not \code{+proj=longlat +datum=WGS84 +no_defs}, it is reprojected to the latter. Use \link{set_aoi} instead to once define an AOI globally for all queries within the running session. If \code{aoi} is undefined, the AOI that has been set using \link{set_aoi} is used.
-#' @param maxDeviation numeric, the maximum allowed deviation of calculated scene cloud cover from the provided scene cloud cover. Use 100 if you do not like to consider the cloud cover % given by the data distributor. Default is \code{maxDeviation = 20}.
-#' @param sceneCloudCoverCol string, the clear name of the cloud cover column in the \code{record} data.frame provided by the data distributor.
-#' @param cloudPrbThreshold numeric, the threshold of the HOT cloud probability layer (0-100 %) below which pixels are considered as clear sky. Default is \code{cloudPrbThreshold = 40}. It will be dynamically adjusted according to the input in \code{maxDeviation}
-#' @param dir_out character, optional. Full path to target directory where to save the cloud masks. If \code{NULL}, cloud masks are not saved.
-#' @param slopeDefault numeric, value taken as slope ONLY if least-alternate deviation regression fails.  Default is 1.4, proven to work well for common land surfaces.f default values. In this case cloud cover will be set to 9999 % for the given record.
-#' @param interceptDefault, value taken as intercept ONLY if least-alternate deviation regression fails. Default is -10, proven to work well for common land surfaces.
-#' @param verbose logical, if \code{TRUE}, details on the function's progress will be visibile on the console. Default is TRUE.
-#'    
+#' @inheritParams calcSentinel_aoi_cloudcov
+#'        
 #' @return A data.frame, one line as the input with one additional column holding the estimated cloud cover within the aoi
 #' 
 #' @author Henrik Fisser
@@ -29,7 +20,7 @@
 #' 
 #' @export
 
-calc_hot_cloudcov <- function(record, preview, aoi = NULL, maxDeviation = 20, sceneCloudCoverCol, cloudPrbThreshold = 40, dir_out = NULL, num_records = 1, slopeDefault = 1.4, interceptDefault = -10, verbose = TRUE) {
+calc_hot_cloudcov <- function(record, preview, aoi = NULL, maxDeviation = 20, sceneCloudCoverCol, cloudPrbThreshold = 40, slopeDefault = 1.4, interceptDefault = -10, dir_out = NULL, num_records = 1, verbose = TRUE) {
   
   AOIcloudcoverpercentage <- "AOIcloudcoverpercentage" # for aoi cloud cover column
   error <- "try-error"
@@ -143,7 +134,7 @@ calc_hot_cloudcov <- function(record, preview, aoi = NULL, maxDeviation = 20, sc
   cMask <- mask(cMask,aoi)
   if (!is.null(dir_out)) { # save cloud mask if desired
     maskFilename <- paste0(dir_out,"\\",records[x,1],"_cloud_mask.tif")
-    raster::writeRaster(cMask,maskFilename,"GTiff",overwrite=T)
+    writeRaster(cMask,maskFilename,"GTiff",overwrite=T)
   }
   cMaskMatAoi <- as.matrix(cMask)
   cPercent <- (length(which(cMaskMatAoi==0)) / length(which(!is.na(cMaskMatAoi)))) * 100 # aoi cc %

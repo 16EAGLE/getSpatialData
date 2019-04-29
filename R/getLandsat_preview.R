3517762eda62c527d6f9ff2b42da39ae221adf83
hfisser <henrik.fisser@t-online.de>
2019-04-29 02:35:46 +0200

    projection

--- R/getLandsat_preview.R
+++ R/getLandsat_preview.R
@@ -59,12 +59,11 @@
 
 getLandsat_preview <- function(record, on_map = TRUE, show_aoi = TRUE, return_preview = FALSE, verbose = TRUE){
   
-  preview_crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-  
   if(isTRUE(return_preview)) {
-    r.prev <- .EE_preview(record = record, preview_crs = preview_crs, on_map = on_map, show_aoi = show_aoi, return_preview = return_preview, verbose = verbose)
+    r.prev <- .EE_preview(record = record, preview_crs = NULL, on_map = on_map, show_aoi = show_aoi, return_preview = return_preview, verbose = verbose)
+    crs(r.prev) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
     return(r.prev)
   } else {
-    .EE_preview(record = record, preview_crs = preview_crs, on_map = on_map, show_aoi = show_aoi, return_preview = return_preview, verbose = verbose)
+    .EE_preview(record = record, preview_crs = NULL, on_map = on_map, show_aoi = show_aoi, return_preview = return_preview, verbose = verbose)
   }
 }

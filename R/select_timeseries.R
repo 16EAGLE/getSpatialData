462d2eb15f288f9ea17be51128285725d10b29b4
hfisser <henrik.fisser@t-online.de>
2019-07-13 11:04:47 +0200

    added preview and cloud mask mosaic creation

--- R/select_timeseries.R
+++ R/select_timeseries.R
@@ -45,7 +45,10 @@ select_timeseries <- function(records, aoi, dir_out = NULL,
   timestamp_col <- "selected_for_timestamp" # the timestamp number for which the record is selected
   identifier <- 1
   sensor <- unique(records$sensor)
-  par <- .get_pars(records)
+  par <- list()
+  par$date_col <- "acquisitionDate"
+  par$period <- .identify_period(records[[date_col]])
+  par$sensor <- records$sensor[1]
   .select_handle_revisit(par$sensor,par$period,num_timestamps) # communicate to the user in case max_period is in conflict with sensor revisit time
   identifier <- .select_handle_landsat(records,par$sensor)[[2]]
   records <- .select_handle_landsat(records,par$sensor)[[1]]
@@ -55,18 +58,16 @@ select_timeseries <- function(records, aoi, dir_out = NULL,
   par$tileid_col <- "tile_id"
   par$preview_col <- "preview_file"
   par$cloud_mask_col <- "Cloud_mask_path"
-  selected <- list() # list to be filled by all selected 'entity_id' ids
+  selected <- list() # list to be filled by all selected 'entity_id' ids, the valid coverage percentage per timestamp and the cloud mask paths
   # select per sub-period best mosaic 
   # Select as the first record of the first sub-period the records with lowest aoi cloud cover that are still within max_period 
   first <- new.env()
   first$records <- records[records$Sub_period == 1,]
   first$period <- .identify_period(first$records[[date_col]])
   # run the selection process
-  first$selected <- .select_process_sub(records=first$records,first$period,par=par,
-                                        tileid_col=tileid_col,
+  first$selected <- .select_process_sub(records=first$records,first$period,max_sub_period,
                                         max_cloudcov_tile=max_cloudcov_tile,
-                                        cloud_mask_col=par$cloud_mask_col,
-                                        dir_out=dir_out,identifier=identifier)
+                                        par=par,dir_out=dir_out,identifier=identifier)
   first$records_sel <- records[match(first$selected$ids,records$entityId),]
   first$period_new <- .identify_period(first$records_sel[[date_col]])
   selected[[1]]$timestamp <- 1
@@ -82,38 +83,28 @@ select_timeseries <- function(records, aoi, dir_out = NULL,
     other$period <- .select_handle_next_sub(first_date=other$first_date,
                                             period_initial=other$period_inital,
                                             min_distance,max_sub_period)
-    other$records <- .within_period(records,other$period,date_col)
+    other$records <- .within_period(records,other$period,date_col) # subset to records within period
     # run the selection process
-    other$selected <- .select_process_sub(other$records,other$period,par=par,
-                                        max_cloudcov_tile=max_cloudcov_tile,
-                                        cloud_mask_col=par$cloud_mask_col,
-                                        dir_out=dir_out,identifier=identifier)
+    other$selected <- .select_process_sub(other$records,other$period,max_sub_period,
+                                          max_cloudcov_tile=max_cloudcov_tile,
+                                          par=par,dir_out=dir_out,identifier=identifier)
     selected[[i]]$timestamp <- i
     selected[[i]] <- other$selected # insert 'selected' object into selected list
   }
   #A Create and save final cloud mask mosaic
   #B Create and save final RGB preview mosaic
-  #C Add 3 columns to records data.frame:
+  #C Add 4 columns to records data.frame:
   #1 logical column if a record is selected at all
   #2 path to the RGB mosaic tif where record is included
   #3 the timestamp number for which the record is selected
   console_info <- c()
   for (i in 1:length(selected)) {
     s <- selected[[i]]
-    id_sel <- sapply(s$ids,function(x) {which(records[,identifier]==x)})
     #A cloud mask mosaic
-    save_path_cmos <- file.path(dir_out,paste0("cloud_mask_mosaic_timestamp",s$timestamp,".tif"))
-    cMask_mosaic <- .select_bridge_mosaic(s$cMask_paths,aoi,dir_out)
-    #B RGB preview mosaic
-    save_path_pmos <- file.path(dir_out,paste0("preview_mosaic_timestamp",s$timestamp,".tif"))
-    # mask preview tiles by cloud masks
-    preview_paths <- sapply(id_sel,function(i) {
-      preview <- raster(records[i,par$preview_col])
-      cMask <- raster(records[i,par$])
-      preview_masked <- .mask_raster(preview,cMask)
-    })
-    preview_mosaic <- .select_bridge_mosaic(preview_paths,aoi,dir_out,mode="rgb")
-    #C add 3 columns to records
+    save_path_cmos <- .select_cmask_mos(s,aoi,dir_out)
+    #B preview RGB mosaic
+    save_path_pmos <- .select_preview_mos(s,aoi,i,identifier,dir_out,par$cloud_mask_col,par$preview_col)
+    #C add 4 columns to records
     records[id_sel,selected_col_name] <- s$ids
     records[id_sel,timestamp_col] <- s$timestamp
     records[id_sel,pmos_col] <- save_path_pmos
@@ -132,3 +123,34 @@ select_timeseries <- function(records, aoi, dir_out = NULL,
   
   return(records)
 }
+
+
+
+
+test <- r[[3]] != 0
+test[test==0] <- NA
+ext <- extent(test)
+poly <- as(ext,"SpatialPolygons")
+crs(poly) <- crs(test)
+coords <- slot(slot(slot(poly, "polygons")[[1]], "Polygons")[[1]], "coords")
+coords[1,1] <- coords[1,1] + 0.083
+coords[2,1] <- coords[2,1] + 0.45
+coords[3,1] <- coords[3,1] - 0.054
+coords[4,1] <- coords[4,1] - 0.38
+coords[5,1] <- coords[5,1] + 0.083
+coords[1,2] <- coords[1,2] + 0.29
+coords[2,2] <- coords[2,2] - 0.036
+coords[3,2] <- coords[3,2] - 0.29
+coords[4,2] <- coords[4,2] + 0.04
+coords[5,2] <- coords[5,2] + 0.29
+coords <- coords_archive
+slot(slot(slot(poly, "polygons")[[1]], "Polygons")[[1]], "coords") <- coords
+p(test)
+plot(poly,add=T)
+
+
+
+
+
+
+

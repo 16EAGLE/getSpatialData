ff9851a86f599eafed642977af0af050af66a0f5
hfisser <henrik.fisser@t-online.de>
2020-03-30 11:52:42 +0200

    minor simplifications and fixes

--- tests/testthat/test-calc_cloudcov.R
+++ tests/testthat/test-calc_cloudcov.R
@@ -3,6 +3,8 @@
 # -----------------------------------------------------------------------------------------
 
 dir_records <- tt$resources$records
+ADDED_COLS_PREVIEWS <- 5
+ADDED_COLS_WITHOUT_PREVIEWS <- 3
 
 # -----------------------------------------------------------------------------------------
 # DEFINE TEST FUNCTIONS
@@ -19,7 +21,7 @@ clean_test_calc_cloudcov <- function(records_cc, COLS, DATAFRAME, NUMERIC, CHARA
   expect_true(COLS$cmask_tif %in% cols_given)
   expect_true(COLS$preview_jpg %in% cols_given)
   expect_true(COLS$preview_tif %in% cols_given)
-  # get column vector
+  # get column vectors
   aoi_cc <- records_cc[[COLS$HOT_aoi]]
   scene_cc <- records_cc[[COLS$HOT_scene]]
   cmask_tifs <- records_cc[[COLS$cmask_tif]]
@@ -47,7 +49,7 @@ clean_test_calc_cloudcov <- function(records_cc, COLS, DATAFRAME, NUMERIC, CHARA
 error_test_calc_cloudcov <- function(records, aoi, tt) {
   
   # records type
-  expect_error(calc_cloudcov(records = "Dumbledore"), dir_out = tt$tmp, RECORDS_TYPE_ERROR)
+  expect_error(calc_cloudcov(records = "Dumbledore", aoi = aoi, dir_out = tt$tmp), RECORDS_TYPE_ERROR)
   # records column missing
   needed_cols <- getSpatialData:::.get_needed_cols_calc_cloudcov()
   for (col_remove in needed_cols) {
@@ -57,25 +59,26 @@ error_test_calc_cloudcov <- function(records, aoi, tt) {
   }
   # aoi
   expect_error(calc_cloudcov(records, aoi = "Forbidden Forest", dir_out = tt$tmp), AOI_TYPE_ERROR)
+  expect_error(calc_cloudcov(records,aoi = NULL, dir_out = tt$tmp), AOI_UNDEFINED_ERROR)
   # dir_out
   input1_dir_out <- 10
-  error1_dir_out <- type_error_msg(input1_dir_out)
+  error1_dir_out <- type_error_msg(input1_dir_out, "dir_out", CHARACTER)
   input2_dir_out <- "Lord Voldemort"
   error2_dir_out <- dir_out_error_msg(input2_dir_out) # dir does not exist message
-  expect_error(calc_cloudcov(records, aoi, dir_out = input1_dir_out), error1_dir_out)
-  expect_error(calc_cloudcov(records, aoi, dir_out = input2_dir_out), error2_dir_out)
+  expect_error(calc_cloudcov(records, aoi = aoi, dir_out = input1_dir_out), error1_dir_out)
+  expect_error(calc_cloudcov(records, aoi = aoi, dir_out = input2_dir_out), error2_dir_out)
   # username
   input_username <- 11
   error_username <- type_error_msg(input_username, "password", CHARACTER)
-  expect_error(calc_cloudcov(records, aoi, dir_out = tt$tmp, username = input_username), error_username)
+  expect_error(calc_cloudcov(records, aoi = aoi, dir_out = tt$tmp, username = input_username), error_username)
   # password
   input_password <- 12
   error_password <- type_error_msg(input_password, "username", CHARACTER)
-  expect_error(calc_cloudcov(records, aoi, dir_out = tt$tmp, password = input_password), error_password)
+  expect_error(calc_cloudcov(records, aoi = aoi, dir_out = tt$tmp, password = input_password), error_password)
   # verbose
   input_verbose = "Avada Kedavra"
   error_verbose <- type_error_msg(input_verbose, "verbose", LOGICAL)
-  expect_error(calc_cloudcov(records, aoi, dir_out = tt$tmp, verbose = input_verbose), error_verbose)
+  expect_error(calc_cloudcov(records, aoi = aoi, dir_out = tt$tmp, verbose = input_verbose), error_verbose)
   
 }
 
@@ -95,7 +98,7 @@ error_test_calc_cloudcov <- function(records, aoi, tt) {
 # Target: Test errors (sensor does not matter)
 records <- read.csv(construct_filepath(dir_records, SENTINEL2, SUFFIX$records))
 # with false input
-error_test_calc_cloudcov(records, aoi_tunisia, tt)
+error_test_calc_cloudcov(records, aoi = aoi_tunisia, tt)
 # calc_hot_cloudcov() with modified preview
 initialize_dir(tt$tmp)
 record_preview <- read.csv(construct_filepath(dir_records, SENTINEL2, SUFFIX$previews))[1,] # one line
@@ -116,12 +119,12 @@ finish_dir(tt$tmp)
 # Target: Test with Sentinel-2
 # provide a records csv WITHOUT previous get_previews() call
 records <- read.csv(construct_filepath(dir_records, SENTINEL2, SUFFIX$records))
-records_cc1 <- expect_is(calc_cloudcov(records, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+records_cc1 <- expect_is(calc_cloudcov(records, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
 # provide a records csv WITH previous get_previews() call
 records_previews <- read.csv(construct_filepath(dir_records, SENTINEL2, SUFFIX$previews))
-records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-expect_length(names(records_cc1), length(records) + 5)
-expect_length(names(records_cc2), length(records) + 3)
+records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+expect_length(names(records_cc1), length(records) + ADDED_COLS_PREVIEWS)
+expect_length(names(records_cc2), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
 for (records_cc in list(records_cc1, records_cc2)) {
   initialize_dir(tt$tmp)
   clean_test_calc_cloudcov(records_cc, COLS, DATAFRAME, NUMERIC, CHARACTER)
@@ -133,10 +136,10 @@ for (records_cc in list(records_cc1, records_cc2)) {
 # Target: Test with Landsat
 records <- read.csv(construct_filepath(dir_records, LANDSAT, SUFFIX$records))
 records_previews <- read.csv(construct_filepath(dir_records, LANDSAT, SUFFIX$previews))
-records_cc1 <- expect_is(calc_cloudcov(records, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-expect_length(names(records_cc1), length(records) + 5)
-expect_length(names(records_cc2), length(records) + 3)
+records_cc1 <- expect_is(calc_cloudcov(records, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+expect_length(names(records_cc1), length(records) + ADDED_COLS_PREVIEWS)
+expect_length(names(records_cc2), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
 
 for (records_cc in list(records_cc1, records_cc2)) {
   initialize_dir(tt$tmp)
@@ -149,10 +152,10 @@ for (records_cc in list(records_cc1, records_cc2)) {
 # Target: Test with MODIS
 records <- read.csv(construct_filepath(dir_records, MODIS, SUFFIX$records))
 records_previews <- read.csv(construct_filepath(dir_records, MODIS, SUFFIX$previews))
-records_cc1 <- expect_is(calc_cloudcov(records, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-expect_length(names(records_cc1), length(records) + 5)
-expect_length(names(records_cc2), length(records) + 3)
+records_cc1 <- expect_is(calc_cloudcov(records, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+expect_length(names(records_cc1), length(records) + ADDED_COLS_PREVIEWS)
+expect_length(names(records_cc2), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
 for (records_cc in list(records_cc1, records_cc2)) {
   initialize_dir(tt$tmp)
   clean_test_calc_cloudcov(records_cc, COLS, DATAFRAME, NUMERIC, CHARACTER)
@@ -164,10 +167,10 @@ for (records_cc in list(records_cc1, records_cc2)) {
 # Target: Test with Sentinel-3
 records <- read.csv(construct_filepath(dir_records, SENTINEL3, SUFFIX$records))
 records_previews <- read.csv(construct_filepath(dir_records, SENTINEL3, SUFFIX$previews))
-records_cc1 <- expect_is(calc_cloudcov(records, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-expect_length(names(records_cc1), length(records) + 5)
-expect_length(names(records_cc2), length(records) + 3)
+records_cc1 <- expect_is(calc_cloudcov(records, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+expect_length(names(records_cc1), length(records) + ADDED_COLS_PREVIEWS)
+expect_length(names(records_cc2), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
 for (records_cc in list(records_cc1, records_cc2)) {
   initialize_dir(tt$tmp)
   clean_test_calc_cloudcov(records_cc, COLS, DATAFRAME, NUMERIC, CHARACTER)
@@ -179,10 +182,10 @@ for (records_cc in list(records_cc1, records_cc2)) {
 # Target: Test sensors mixed sensors including SAR
 records <- read.csv(construct_filepath(dir_records, MIXED, SUFFIX$records))
 records_previews <- read.csv(construct_filepath(dir_records, MIXED, SUFFIX$previews))
-records_cc1 <- expect_is(calc_cloudcov(records, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
-expect_length(names(records_cc1), length(records) + 5)
-expect_length(names(records_cc2), length(records) + 3)
+records_cc1 <- expect_is(calc_cloudcov(records, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+records_cc2 <- expect_is(calc_cloudcov(records_previews, aoi = aoi_tunisia, dir_out = tt$tmp), DATAFRAME)
+expect_length(names(records_cc1), length(records) + ADDED_COLS_PREVIEWS)
+expect_length(names(records_cc2), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
 for (records_cc in list(records_cc1, records_cc2)) {
   initialize_dir(tt$tmp)
   clean_test_calc_cloudcov(records_cc, COLS, DATAFRAME, NUMERIC, CHARACTER)

d2171f835f5fd6b4883ac0b12cd7dc6a4f27cd74
hfisser <henrik.fisser@t-online.de>
2020-04-01 18:08:44 +0200

    initialized internal_names for generic naming in getSpatialData

---
+++ R/internal_names.R
@@ -0,0 +1,7 @@
+#' ---------------------------------------------------------------------
+#' @name internal_names
+#' @description These are functions that return names used in getSpatialData.
+#' The purpose is generic naming and simple maintenance.
+#' @keywords internal
+#' @noRd
+#' ---------------------------------------------------------------------

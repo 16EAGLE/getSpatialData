c06ed05e85cd746886e5de233ffa24787c811062
hfisser <henrik.fisser@t-online.de>
2020-04-02 19:34:44 +0200

    renaming

--- R/get_cloudcov_supported.R
+++ R/get_cloudcov_supported.R
@@ -1,11 +1,11 @@
 #' Returns product names supported by \code{calc_cloudcov}
 #' 
-#' @description \code{cloudcov_supported} provides all product names supported by \link{calc_cloudcov}.
+#' @description \code{get_cloudcov_supported} provides all product names supported by \link{calc_cloudcov}.
 #' Note that among the Sentinel-3 sensors only OLCI products are supported.
 #' 
 #' @return character vector of product names supported by \link{calc_cloudcov}.
 #' @author Henrik Fisser, 2020
 #' @export
-cloudcov_supported <- function() {
+get_cloudcov_supported <- function() {
   return(.cloudcov_products())
 }
---
+++ R/get_select_supported.R
@@ -0,0 +1,19 @@
+#' Returns product names supported by \code{select_*} functionalities
+#' 
+#' @description \code{get_select_supported} provides all product names supported by:
+#' \itemize{
+#' \item \link{select_unitemporal}
+#' \item \link{select_bitemporal}
+#' \item \link{select_timeseries}
+#' }
+#' Other products than the returned cannot be processed by \code{select_*}.
+#' Note that among the Sentinel-3 sensors only OLCI products are supported.
+#' 
+#' @return character vector of product names supported by \code{select_*}.
+#' @author Henrik Fisser, 2020
+#' @export
+get_select_supported <- function() {
+  optical_sensors <- .cloudcov_products()
+  supported_products <- c(optical_sensors, "Sentinel-1")
+  return(supported_products)
+}

04110a53e282d6685f81ac791a6b57bd17f48665
hfisser <henrik.fisser@t-online.de>
2020-04-15 16:40:41 +0200

    - user doc
    - error handling

--- R/read_records.R
+++ R/read_records.R
@@ -3,33 +3,31 @@
 #' @description \code{read_records} reads records from a csv file
 #' that has been previously written through \link{write_records}.
 #' 
-#' @param file character complete file path from where to read the records csv.
-#' @param as_sf logical specify if the records shall be returned as sf object.
+#' @param file character complete file path from where to read the records file.
+#' @param as_sf logical specifies if the records shall be returned as sf object.
 #' Otherwise a data.frame will be returned.
 #' 
 #' @return sf object or data.frame depending on \code{as_sf}.
 #' 
-#' @importFrom readr read_csv cols
-#' 
 #' @author Henrik Fisser, 2020
 #' 
 #' @export
-read_records <- function(file, as_sf = TRUE) {
+read_records <- function(file, as_sf = TRUE, verbose = TRUE) {
   
+  .check_verbose(verbose)
   .check_character(file, "file")
-  exists <- .is_existing_csv_file(file, out_type = 3) # throws error in case of FALSE
-  rm(exists)
-  # it is important to write read through readr::read_csv() for correct representation
-  # of the records, especially of its footprints
-  records <- try(as.data.frame(read_csv(file, col_types = cols())))
-  is_dataframe <- inherits(records, "data.frame")
-  if (is_dataframe) {
+  .check_file_exists(file, 3)
+  records <- try(st_read(file, quiet = !verbose))
+  has_dataframe <- inherits(records, "data.frame") # sf dataframe
+  if (has_dataframe) {
     records <- .eval_records_footprints(records, as_sf = as_sf)
     records <- .df_dates_to_chars(records)
     records <- .unlist_df(records)
     return(records)
   } else {
-    out("Reading records csv was unsuccessful", 3)
+    out(paste0("Failed to read records: ", file), 3)
   }
+  
+  names(records)[which(names(records) == "geom")] <- name_footprint()
 
 }
--- R/write_records.R
+++ R/write_records.R
@@ -1,54 +1,51 @@
 #' Writes records to a file
 #' 
-#' @description \code{write_records} write records to a file
-#' that has been previously written through \link{write_records}.
+#' @description \code{write_records} write records to a file.
 #' 
 #' @inheritParams get_records
 #' 
-#' @param dir_out directory where to write the records. A file name will be automatically
-#' generated from date and time in case \code{dir_out} is set through \link{set_archive} or provided. 
-#' Default is NULL.
-#' @param file character complete file path where to write the records. Default is NULL.
-#' If a \code{dir_out} is not NULL because it is set through \link{set_archive} or provided, 
-#' \code{file} will be ignored.
+#' @param file_name character name of the file to be saved. If it is NULL a file name will be 
+#' automatically generated from date and time.
 #' @param driver character specifies the writer. Options are all returned by
-#' sf::st_drivers() and "csv". Default is "GPKG".
-#' @param append logical if TRUE and a file path is given through \code{file} the record(s)
+#' sf::st_drivers() and "csv". Will be ignored in case a fully qualified
+#' \code{file} path is provided. Default is "GPKG"
+#' @param file character complete file path where to write the records. Must be a full file path
+#' including file basename and extension matching a supported driver. Default is NULL.
+#' @param append logical, if TRUE and a file path is given through \code{file}, the record(s)
 #' will be appended to \code{file} if possible.
 #'  
-#' @return character file path to the saved records csv.
+#' @return character file path to the saved records.
 #' 
-#' @importFrom readr write_csv
 #' @importFrom sf st_write
 #' 
 #' @author Henrik Fisser, 2020
 #' 
 #' @export
-write_records <- function(records, dir_out = NULL, file = NULL, driver = "GPKG", append = FALSE) {
+write_records <- function(records, file_name = NULL, driver = "GPKG", dir_out = NULL, 
+                          file = NULL, append = FALSE, verbose = TRUE) {
   
+  .check_verbose(verbose)
   .check_gdal_driver(driver)
   file_given <- !is.null(file)
-  as_csv <- ifelse(file_given, endsWith(file, ".csv"), endsWith(tolower(driver), "csv"))
-  
+  if (file_given) driver = NA
+
   .check_records_type(records)
   records <- .unlist_df(records)
   # if no file given we have to check dir_out
   if (!file_given) {
     dir_out <- .check_dir_out(dir_out) # can throw error
-    ext <- ifelse(as_csv, ".csv", .get_driver_extension(driver))
-    filename <- .generate_datetime_filename("records", extension = ext)
-    file <- file.path(dir_out, filename)
+    ext <- .get_driver_extension(driver)
+    if (is.null(file_name)) file_name <- .generate_datetime_filename("records", extension = ext)
+    file <- file.path(dir_out, file_name)
   } 
   
-  if (as_csv) {
-    write_csv(records, file, append = append)
+  records <- .check_records(records) # ensure it's sf
+  if (is.na(driver)) {
+    write <- try(st_write(records, dsn = file, append = append, quiet = !verbose))
   } else {
-    records <- .check_records(records) # ensure it's sf
-    st_write(records, dsn = file, driver = driver, append = append)
-    test <- st_read(file)
-    
+    write <- try(st_write(records, dsn = file, driver = driver, append = append, quiet = !verbose))
   }
-  
+  if (inherits(write, "try-error")) out(paste0("Failed to write file: ", file), 3)
   return(file)
   
 }

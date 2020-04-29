d6dd86bce20a4a60988732eb0089076b538858fc
hfisser <henrik.fisser@t-online.de>
2020-04-29 12:06:02 +0200

    console communicate when processing cloud mask of yet processed record

--- R/calc_cloudcov.R
+++ R/calc_cloudcov.R
@@ -189,13 +189,15 @@ calc_cloudcov <- function(records, max_deviation = 5,
     # otherwise run HOT afterwards
     record_path <- .generate_records_filename(file_name = id, dir_out = dir_out, driver = driver)
     if (.check_file_exists(record_path)) {
-      out(paste0(out_status,"Loading yet processed record: ", id), msg=T, verbose=v)
+      out(paste0(out_status,"Loading yet processed record: ", id), msg = T, verbose=v)
       record <- read_records(record_path, as_sf = F, verbose = FALSE)
       nms <- names(record)
       if (cloud_mask_file %in% nms && preview_file %in% nms &&
           NROW(record) > 0) {
         if (.check_file_exists(record[[cloud_mask_file]])) {
           return(record)
+        } else {
+          out("Calculating cloud mask of yet processed record", msg = T)
         }
       }
     } else {

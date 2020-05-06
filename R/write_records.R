d97d231ca0d2f632e6965d897b1b909f35040b7e
hfisser <henrik.fisser@t-online.de>
2020-05-06 17:57:21 +0200

    simplified write_records

--- R/write_records.R
+++ R/write_records.R
@@ -1,17 +1,16 @@
 #' Writes records to a file
 #' 
-#' @description \code{write_records} write records to a file. They can be read through
-#' \link{read_records}.
+#' @description \code{write_records} Write records to a file. Obtain available formats through \link{get_records_drivers}.
+#' Records can be read through \link{read_records}.
 #' 
 #' @inheritParams calc_cloudcov
 #' 
-#' @param file_name character name of the file to be saved. If it is NULL a file name will be 
-#' automatically generated from date and time.
+#' @param file character file name or file path of file to be saved. If it is a file name it will be written
+#' at \code{dir_out} in the format of \code{driver}. If it is a file path without valid file extension 
+#' it will be written in the format of \code{driver} at the specified path. If it is a file path with 
+#' valid file extension it will be written there. Check for available drivers and extensions through \link{get_records_drivers}.
 #' @param driver character specifies the writer. Options are all thise returned by
-#' sf::st_drivers() plus "csv". Will be ignored in case a fully qualified
-#' \code{file} path is provided. Default is "GPKG"
-#' @param file character complete file path where to write the records. Must be a full file path
-#' including file basename and extension matching a supported driver. Default is NULL.
+#' \link{get_records_drivers}. Will be ignored in case a fully qualified \code{file} path is provided. Default is "GPKG".
 #' @param append logical, if TRUE and a file path is given through \code{file}, the record(s)
 #' will be appended to \code{file} if possible.
 #' @param ... further arguments to be passed to sf::st_write.
@@ -23,28 +22,31 @@
 #' @author Henrik Fisser, 2020
 #' 
 #' @export
-write_records <- function(records, file_name = NULL, driver = "GPKG", dir_out = NULL, 
-                          file = NULL, append = FALSE, verbose = TRUE, ...) {
+write_records <- function(records, file = NULL, driver = "GPKG", dir_out = NULL, 
+                          append = FALSE, verbose = TRUE, ...) {
   
   if (is.null(append)) append <- FALSE # could be passed as NULL through ... from higher level
   .check_verbose(verbose)
   .check_gdal_driver(driver)
-  file_given <- !is.null(file)
-  if (file_given) driver <- NA
   .check_records_type(records)
-  records <- .unlist_df(records)
-  # if no file given we have to check dir_out
-  if (!file_given) {
-    dir_out <- .check_dir_out(dir_out) # can throw error
-    file <- .generate_records_filename(file_name = file_name, dir_out = dir_out, driver = driver)
-  } 
   records <- .check_records(records) # ensure it's sf
+  records <- .unlist_df(records)
+  file_is_path <- !is.null(file) && file != basename(file)
+  if (!file_is_path) {
+    dir_out <- .check_dir_out()
+    # generate a file path from dir_out
+    file <- ifelse(file_is_path, .generate_records_filename(file_name = file, dir_out = dir_out, driver = driver), file)
+  }
+  # check if file ends with a valid file extension
+  split <- strsplit(file, "\\.")[[1]]
+  ext <- split[length(split)]
   out(paste0("Writing records to ", file), msg=T, type=1)
-  if (is.na(driver)) {
+  if (ext %in% .get_records_drivers()) {
     write <- try(st_write(records, dsn = file, append = append, quiet = TRUE))
   } else {
     write <- try(st_write(records, dsn = file, driver = driver, append = append, quiet = TRUE))
   }
+  
   if (inherits(write, "try-error")) {
     out(paste0("Failed to write file: ", file), 3)
   } else {
@@ -52,4 +54,13 @@ write_records <- function(records, file_name = NULL, driver = "GPKG", dir_out = 
   }
   return(file)
   
+}
+
+#' Returns drivers for writing records
+#' @description \code{get_records_drivers} provides the driver names that can be used in \link{write_records}.
+#' @return list (named) of characters drivers (names) and their extensions (values).
+#' @author Henrik Fisser, 2020
+#' @export
+get_records_drivers <- function() {
+  return(.get_records_drivers())
 }

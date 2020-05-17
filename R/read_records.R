a4ccb68566f488862231a68ff358cc55b74a2eaa
hfisser <henrik.fisser@t-online.de>
2020-05-17 12:02:37 +0200

    geometry column may have different names according to driver

--- R/read_records.R
+++ R/read_records.R
@@ -1,7 +1,7 @@
-#' read records from file
+#' Read records from file
 #' 
-#' @description \code{read_records} reads records from a csv file
-#' that has been previously written through \link{write_records}.
+#' @description \code{read_records} reads records that have been 
+#' written through \link{write_records}.
 #' 
 #' @param file character file path from where to read the records file.
 #' @param as_sf logical specifies if the records shall be returned as sf object.
@@ -17,6 +17,9 @@
 #' @export
 read_records <- function(file, as_sf = TRUE, verbose = TRUE) {
   GEOM <- "geom"
+  GEOMETRY <- "geometry"
+  GEOMETRY_PROP <- "geometryProperty"
+  geoms <- c(GEOM, GEOMETRY, GEOMETRY_PROP)
   FOOTPRINT <- name_footprint()
   .check_as_sf(as_sf)
   .check_verbose(verbose)
@@ -25,14 +28,16 @@ read_records <- function(file, as_sf = TRUE, verbose = TRUE) {
   out(paste0("Reading records from ", file), msg=T, type=1)
   records <- try(suppressWarnings(st_read(file, stringsAsFactors = FALSE, quiet = TRUE)))
   if (inherits(records, "data.frame")) { # sf dataframe
-    if (as_sf && endsWith(tolower(file), ".csv")) out("Converting to sf data.frame", msg = T, type = 1)
-    if (GEOM %in% names(records)) {
+    names <- names(records)
+    if (any(geoms %in% names)) {
+      if (as_sf && endsWith(tolower(file), ".csv")) out("Converting to sf data.frame", msg = T, type = 1)
       records[[FOOTPRINT]] <- NULL
-      names(records)[which(names(records) == GEOM)] <- FOOTPRINT
+      names(records)[which(names(records) == geoms[geoms %in% names])] <- FOOTPRINT
+      st_geometry(records) <- FOOTPRINT
+      records <- .eval_records_footprints(records, as_sf = as_sf)
     }
-    st_geometry(records) <- FOOTPRINT
+    if (as_sf && !inherits(records, "sf")) out(paste0("Cannot convert to sf data.frame"), msg = T, type = 2)
     records <- .uncharacter_dataframe(records)
-    records <- .eval_records_footprints(records, as_sf = as_sf)
     records <- .unlist_df(records)
     n_records <- NROW(records)
     out(paste0("Read ", 

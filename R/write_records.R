7495fe9b53aeff7d2524c36ee9684a3e00064664
hfisser <henrik.fisser@t-online.de>
2020-05-17 11:59:26 +0200

    - changed default driver to GeoJSON due to less space needed
    - handle case where user does not provide driver name but file extension
    
    - suppressWarnings of st_write

--- R/write_records.R
+++ R/write_records.R
@@ -1,6 +1,6 @@
-#' Writes records to file
+#' Write records to file
 #' 
-#' @description \code{write_records} Write records to a file. Obtain available formats through \link{get_records_drivers}.
+#' @description \code{write_records} writes records. Obtain available formats through \link{get_records_drivers}.
 #' Records can be read through \link{read_records}.
 #' 
 #' @inheritParams calc_cloudcov
@@ -8,23 +8,26 @@
 #' @param file character file name or file path of file to be saved. If it is a file name it will be written
 #' at \code{dir_out} in the format of \code{driver}. If it is a file path without valid file extension 
 #' it will be written in the format of \code{driver} at the specified path. If it is a file path with 
-#' valid file extension it will be written there. Check for available drivers and extensions through \link{get_records_drivers}.
+#' valid file extension it will be written there. It can also be a character file name, in this case 
+#' \code{dir_out} has to be provided. 
+#' Check for available drivers and extensions through \link{get_records_drivers}.
 #' @param driver character specifies the writer. Options are all thise returned by
-#' \link{get_records_drivers}. Will be ignored in case a fully qualified \code{file} path is provided. Default is "GPKG".
+#' \link{get_records_drivers}. Will be ignored in case a fully qualified \code{file} path is provided. Default is "GeoJSON".
 #' @param append logical, if TRUE and a file path is given through \code{file}, the record(s)
 #' will be appended to \code{file} if possible.
 #' @param ... further arguments to be passed to sf::st_write.
 #'  
-#' @return character file path to the saved records.
+#' @return character file path to saved records.
 #' 
 #' @importFrom sf st_write
 #' 
 #' @author Henrik Fisser, 2020
 #' 
 #' @export
-write_records <- function(records, file = NULL, driver = "GPKG", dir_out = NULL, 
+write_records <- function(records, file = NULL, driver = "GeoJSON", dir_out = NULL, 
                           append = FALSE, verbose = TRUE, ...) {
   
+  name_footprint <- name_footprint()
   if (is.null(append)) append <- FALSE # could be passed as NULL through ... from higher level
   .check_verbose(verbose)
   .check_gdal_driver(driver)
@@ -32,30 +35,38 @@ write_records <- function(records, file = NULL, driver = "GPKG", dir_out = NULL,
   records <- .check_records(records) # ensure it's sf
   records <- .unlist_df(records)
   user_file_is_path <- !is.null(file) && file != basename(file)
+  
+  drivers <- get_records_drivers()
+  ifelse(driver %in% names(drivers), driver, names(drivers)[which(drivers == driver)])
+  
+  if (is.null(dir_out) && is.null(file)) {
+    out("One of the two arguments 'file' and 'dir_out' has to be provided", 3)
+  }
+  
   if (!user_file_is_path) {
     dir_out <- .check_dir_out(dir_out)
     # generate a file path from dir_out
-    file <- ifelse(user_file_is_path, .generate_records_filename(file_name = file, 
-                                                                 dir_out = dir_out, driver = driver), file)
+    file <- .generate_records_filename(file_name = file, dir_out = dir_out, driver = driver)
   }
   # check if file ends with a valid file extension
+  if (basename(file) == file) file <- file.path(dir_out, file)
   split <- strsplit(file, "\\.")[[1]]
   ext <- split[length(split)]
   out(paste0("Writing records to ", file), msg=T, type=1)
-  drivers <- get_records_drivers()
-  if (tolower(paste0(".", ext)) %in% tolower(drivers)) {
-    write <- try(st_write(records, dsn = file, append = append, quiet = TRUE))
-  } else {
-    if (!endsWith(file, drivers[[driver]])) file <- paste0(file, drivers[[driver]])
-    write <- try(st_write(records, dsn = file, driver = driver, append = append, quiet = TRUE))
+  if (!any(sapply(drivers, function(x) {endsWith(file, x)}))) {
+    file <- paste0(file, drivers[[driver]])
   }
-  
+  if (tolower(paste0(".", ext)) %in% tolower(drivers)) {
+    write <- try(suppressWarnings(st_write(records, dsn = file, append = append, quiet = TRUE)))
+  } else {
+    write <- try(suppressWarnings(st_write(records, dsn = file, driver = driver, append = append, quiet = TRUE)))
+  }
   if (inherits(write, "try-error")) {
     out(paste0("Failed to write file: ", file), 3)
   } else {
     out(paste0("Wrote ", NROW(records), " records"), msg=F, type=1)
   }
-  return(file)
+  return(normalizePath(file))
 
 }
 

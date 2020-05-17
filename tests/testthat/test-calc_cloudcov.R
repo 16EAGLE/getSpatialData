913a4d8a0708e14d4a611e6b17f9cc16b3f0da73
hfisser <henrik.fisser@t-online.de>
2020-05-18 00:15:06 +0200

    - extended tests - made them more concise

--- tests/testthat/test-calc_cloudcov.R
+++ tests/testthat/test-calc_cloudcov.R
@@ -11,47 +11,70 @@ dir_records <- tt$resources$records
 # tests on the output with clean input
 # ------------------------------------
 clean_test_calc_cloudcov <- function(dir_records, aoi_test, sensor, tt, PREFIX, COLS, NUMERIC, CHARACTER) {
-  ADDED_COLS_PREVIEWS <- 3
-  ADDED_COLS_WITHOUT_PREVIEWS <- 5
   initialize_dir(tt$tmp)
+  ADDED_COLS_CLOUDCOV <- 3
+  ADDED_COLS_PREVIEWS_CLOUDCOV <- 5
   records <- read_records(construct_filepath(dir_records, sensor, PREFIX$cmasks))
-  nrows <- ifelse(NROW(records) > 1, 1:2, 1)
   is_mixed <- sensor == "mixed"
-  nrows <- ifelse(is_mixed, 1:NROW(records), nrows)
+  if (NROW(records) > 1) {
+    if (is_mixed) {
+      nrows <- 1:NROW(records)
+    } else {
+      # vary the number of records to process with from time to time, all have to work
+      nrows <- 1:sample(c(1:NROW(records)), 1)
+    }
+  } else {
+    nrows <- 1
+  }
   records_previews <- set_null_cloudcov_cols(records)
   records <- set_null_preview_cols(records_previews)
-  records_cc1 <- expect_is(calc_cloudcov(records[nrows,], aoi = aoi_test, dir_out = tt$tmp, as_sf = FALSE), DATAFRAME)
-  expect_length(names(records_cc1), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
+  length_base_records <- length(records)
+  # -------
+  # no reload, return as data.frame
+  records_no_reload_df <- expect_is(calc_cloudcov(records[nrows,], 
+                                                  aoi = aoi_test, dir_out = tt$tmp, as_sf = FALSE), DATAFRAME)
+  expect_length(names(records_no_reload_df), length_base_records + ADDED_COLS_PREVIEWS_CLOUDCOV)
+  result_valid_test_calc_cloudcov(records_no_reload_df)
+  # cleanup
   if (!is_mixed) {
-    records_cc2 <- expect_is(calc_cloudcov(records[nrows,], aoi = aoi_test, dir_out = tt$tmp, as_sf = TRUE), SF)
-    expect_length(names(records_cc2), length(records) + ADDED_COLS_WITHOUT_PREVIEWS)
+    finish_dir(tt$tmp)
+    initialize_dir(tt$tmp)
+    # no reload, return as sf
+    records_no_reload_sf <- expect_is(calc_cloudcov(records[nrows,],
+                                                    aoi = aoi_test, dir_out = tt$tmp, as_sf = TRUE), SF)
+    expect_length(names(records_no_reload_sf), length_base_records + ADDED_COLS_PREVIEWS_CLOUDCOV)
+    result_valid_test_calc_cloudcov(records_no_reload_sf)
   }
-  finish_dir(tt$tmp)
-  initialize_dir(tt$tmp)
-  records_cc1 <- expect_is(calc_cloudcov(records_previews[nrows,], aoi = aoi_test, dir_out = tt$tmp, as_sf = TRUE), SF)
-  expect_length(names(records_cc1), length(records_previews) + ADDED_COLS_PREVIEWS)
+  
+  # -------
+  # delete processed cloud masks and process with preview reload
+  delete_files(records_no_reload_df[[COLS$cmask_tif]])
+  # preview reload, return as sf
+  records_previews <- set_null_cloudcov_cols(records_no_reload_df)
+  records_preview_reload <- expect_is(calc_cloudcov(records_previews[nrows,], 
+                                                          aoi = aoi_test, dir_out = tt$tmp, as_sf = TRUE), SF)
+  expect_length(names(records_preview_reload), NCOL(records_previews) + ADDED_COLS_CLOUDCOV)
+  result_valid_test_calc_cloudcov(records_preview_reload)
   if (!is_mixed) {
-    records_cc2 <- expect_is(calc_cloudcov(records_previews[nrows,], aoi = aoi_test, dir_out = tt$tmp, as_sf = FALSE), DATAFRAME)
-    expect_length(names(records_cc2), length(records_previews) + ADDED_COLS_PREVIEWS)
+    # cleanup
+    delete_files(records_no_reload_df[[COLS$cmask_tif]])
+    # preview reload, return as data.frame
+    records_preview_reload <- expect_is(calc_cloudcov(records_no_reload_df[nrows,], 
+                                                         aoi = aoi_test, dir_out = tt$tmp, as_sf = FALSE), DATAFRAME)
+    expect_length(names(records_preview_reload), NCOL(records_previews) + ADDED_COLS_CLOUDCOV)
+    result_valid_test_calc_cloudcov(records_preview_reload)
   }
-  for (records_cc in list(records_cc1, records_cc2)) {
-    cols_given <- names(records_cc)
-    # check if column exists
-    expect_true(COLS$HOT_aoi %in% cols_given)
-    expect_true(COLS$HOT_scene %in% cols_given)
-    expect_true(COLS$cmask_tif %in% cols_given)
-    expect_true(COLS$preview_jpg %in% cols_given)
-    expect_true(COLS$preview_tif %in% cols_given)
-    cmask_tifs <- records_cc[[COLS$cmask_tif]]
-    preview_jpgs <- records_cc[[COLS$preview_jpg]]
-    preview_tifs <- records_cc[[COLS$preview_tif]]
-    # check column data type
-    expect_is(cmask_tifs, CHARACTER)
-    expect_is(preview_jpgs, CHARACTER)
-    expect_is(preview_tifs, CHARACTER)
-    # check cloud mask rasters
-    cloud_masks_test_calc_cloudcov(records_cc)
-  }
+  
+  # -------
+  # delete processed record files and process with cloud mask (and preview) reload
+  del <- sapply(records_no_reload_df[[getSpatialData:::name_record_id()]], function(record_id) {
+    files <- list.files(tt$tmp, pattern=record_id)
+    unlink(file.path(tt$tmp, files[!sapply(files, endsWith, ".tif")]))
+  })
+  rm(del)
+  records_cloud_mask_reload <- expect_is(calc_cloudcov(records[nrows,], aoi = aoi_test, dir_out = tt$tmp, as_sf = TRUE), SF)
+  expect_length(names(records_cloud_mask_reload), NCOL(records) + ADDED_COLS_PREVIEWS_CLOUDCOV)
+  result_valid_test_calc_cloudcov(records_cloud_mask_reload)
   finish_dir(tt$tmp)
 }
 
@@ -89,23 +112,25 @@ error_test_calc_cloudcov <- function(records, aoi, tt) {
   input_verbose = "Avada Kedavra"
   error_verbose <- type_error_msg(input_verbose, "verbose", LOGICAL)
   expect_error(calc_cloudcov(records, aoi = aoi, dir_out = tt$home, verbose = input_verbose), error_verbose)
-  
 }
 
 # tests if cloud masks have right format and values make sense
 # ------------------------------------
 cloud_masks_test_calc_cloudcov <- function(records_cc) {
-  record <- records_cc[1,]
+  row <- sample(1:NROW(records_cc), 1) # take one row but not always the same
+  record <- records_cc[row,]
   cloud_mask_file <- expect_type(record[[COLS$cmask_tif]], CHARACTER)
   is_tif <- endsWith(cloud_mask_file, ".tif")
+  is_none <- endsWith(cloud_mask_file, "NONE")
   cloudcov <- record[[COLS$HOT_scene]]
   aoi_cloudcov <- record[[COLS$HOT_aoi]]
   if (getSpatialData::is.sentinel1(record)) {
     expect_false(is_tif)
+    expect_true(is_none)
     expect_true(is.na(cloudcov))
     expect_true(is.na(aoi_cloudcov))
   } else {
-    expect_true(is_tif)
+    expect_true(is_tif || is_none) # can be NONE
     cloud_mask <- getSpatialData:::.read_brick(cloud_mask_file)
     values <- values(cloud_mask)
     values <- values[!is.na(values)]
@@ -114,32 +139,60 @@ cloud_masks_test_calc_cloudcov <- function(records_cc) {
     expect_true(all(values >= 0))
     expect_is(cloudcov, NUMERIC)
     expect_true(cloudcov >= 0)
-    expect_true(cloudcov <= 100)
+    expect_true(cloudcov <= 100 || cloudcov == 9999)
     expect_is(aoi_cloudcov, NUMERIC)
     expect_true(aoi_cloudcov >= 0)
-    expect_true(aoi_cloudcov <= 100)
+    expect_true(aoi_cloudcov <= 100 || (aoi_cloudcov == 9999 && is_none))
   }
+}
+
+#' tests if result of calc_cloudcov is valid
+# ------------------------------------
+result_valid_test_calc_cloudcov <- function(records_cc) {
+  cols_given <- names(records_cc)
+  # check if column exists
+  expect_true(COLS$HOT_aoi %in% cols_given)
+  expect_true(COLS$HOT_scene %in% cols_given)
+  expect_true(COLS$cmask_tif %in% cols_given)
+  expect_true(COLS$preview_jpg %in% cols_given)
+  expect_true(COLS$preview_tif %in% cols_given)
+  cmask_tifs <- records_cc[[COLS$cmask_tif]]
+  preview_jpgs <- records_cc[[COLS$preview_jpg]]
+  preview_tifs <- records_cc[[COLS$preview_tif]]
+  # check column data type
+  expect_is(cmask_tifs, CHARACTER)
+  expect_is(preview_jpgs, CHARACTER)
+  expect_is(preview_tifs, CHARACTER)
+  # check cloud mask rasters
+  cloud_masks_test_calc_cloudcov(records_cc)
+}
+
+#' helper for deleting vector of files
+delete_files <- function(files) {
+  del <- sapply(files, function(cloud_mask_file) {
+    unlink(cloud_mask_file)
+  })
+  rm(del)
 }
 
 # -----------------------------------------------------------------------------------------
 # RUN TESTS
 # -----------------------------------------------------------------------------------------
 
-# ------------------------------------
-# WITHOUT RELOAD
-# Input: a records data.frame as received from get_records() 
-# Specification: NO reload of previously processed records
-# ------------------------------------
-
 # TEST 1
 # -------
 # Target: Test errors (sensor does not matter)
-records <- read_records(construct_filepath(dir_records, SENTINEL2, PREFIX$records))
+initialize_dir(tt$tmp)
+records <- read_records(construct_filepath(dir_records, SENTINEL2, PREFIX$cmasks))
 # with false input
 error_test_calc_cloudcov(records, aoi = aoi_test, tt)
 # calc_hot_cloudcov() with modified preview
-initialize_dir(tt$tmp)
-record_preview <- read_records(construct_filepath(dir_records, SENTINEL2, PREFIX$cmasks), as_sf = FALSE)[1,] # one line
+record_cloud_mask <- read_records(construct_filepath(dir_records, SENTINEL2, PREFIX$cmasks), as_sf = FALSE)[1,] # one line
+record_preview <- set_null_cloudcov_cols(record_cloud_mask)
+record <- set_null_preview_cols(record_cloud_mask)
+record_cloud_mask <- getSpatialData:::calc_cloudcov(record, aoi = aoi_test, dir_out = tt$tmp)
+unlink(record_cloud_mask$cloud_mask_file)
+record_preview <- set_null_cloudcov_cols(record_cloud_mask)
 preview <- getSpatialData:::.read_brick(record_preview$preview_file)
 input_preview1 <- getSpatialData:::.subset_brick(preview) # blue red band preview (should not work)
 expect_error(getSpatialData:::calc_hot_cloudcov(record_preview, input_preview1, 
@@ -176,3 +229,4 @@ clean_test_calc_cloudcov(dir_records, aoi_test, MODIS, tt, PREFIX, COLS, NUMERIC
 # Target: Test mixed sensors including SAR
 clean_test_calc_cloudcov(dir_records, aoi_test, MIXED, tt, PREFIX, COLS, NUMERIC, CHARACTER)
 
+# -----------------------------------------------------------------------------------------

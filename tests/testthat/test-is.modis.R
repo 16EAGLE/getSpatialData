613980036acd4d33c857df5e2c641c204c2f0cbd
hfisser <henrik.fisser@t-online.de>
2020-06-04 14:59:36 +0200

    test for MODIS aqua terra checkers

--- tests/testthat/test-is.modis.R
+++ tests/testthat/test-is.modis.R
@@ -3,14 +3,25 @@
 
 product_group <- "MODIS"
 product1 <- "MODIS_MOD09A1_V6"
-dummy_records <- data.frame("product" = c(product1, "Sentinel-2", "some_other_product"), 
-                            "product_group" = c(product_group, 
+product2 <- "MODIS_MYD09A1_V6"
+dummy_records <- data.frame("product" = c(product1, product2, "Sentinel-2", "some_other_product"), 
+                            "product_group" = c(product_group, product_group, 
                                                 "Sentinel", "some_other_product_group"),
-                            "record_id" = c("MOD09A1.A2019241.h18v05.006", 
+                            "record_id" = c("MOD09A1.A2019241.h18v05.006", "MYD09A1.A2019241.h18v05.006",
                                             "S2B_MSIL12A_20190905T095031_N0208_R079_T33TWL_20190905T110359", 
                                             "s_omething_else"), stringsAsFactors = F)
 
 # Test is.modis
-should_be <- c(TRUE, FALSE, FALSE)
+should_be <- c(TRUE, TRUE, FALSE, FALSE)
 value <- is.modis(dummy_records)
+expect_equal(value, should_be)
+
+# Test is.modis_terra
+should_be <- c(TRUE, FALSE, FALSE, FALSE)
+value <- is.modis_terra(dummy_records)
+expect_equal(value, should_be)
+
+# Test is.modis_aqua
+should_be <- c(FALSE, TRUE, FALSE, FALSE)
+value <- is.modis_aqua(dummy_records)
 expect_equal(value, should_be)

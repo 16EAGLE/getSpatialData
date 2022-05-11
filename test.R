3a3ea80582b94bd9562c04f461e1a313941db4ad
16eagle <jxsw@web.de>
2022-05-11 17:03:19 +0200

    updated unit tests

---
+++ test.R
@@ -0,0 +1,3 @@
+Sys.setenv(gSD_user = getPass::getPass("Enter gSD user for tests:"))
+Sys.setenv(gSD_pass = getPass::getPass("Enter gSD password for tests:"))
+Sys.setenv(gSD_downtests = FALSE)
--- tests/testthat.R
+++ tests/testthat.R
@@ -5,4 +5,4 @@ library(sf)
 
 Sys.setenv("R_TESTS" = "") ## needed to pass R CMD check: https://github.com/hadley/testthat/issues/144
 
-#test_check("getSpatialData")
+test_check("getSpatialData")

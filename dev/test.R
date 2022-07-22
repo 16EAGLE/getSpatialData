285e24d6931ba8f0fb0325481d5b3e227c7833a0
16EAGLE <jxsw@web.de>
2022-07-22 16:43:05 +0200

    updated tests

--- dev/test.R
+++ dev/test.R
@@ -1,10 +1,9 @@
 # authentificate
 Sys.setenv(gSD_user = getPass::getPass("Enter gSD user for tests:"))
 Sys.setenv(gSD_pass = getPass::getPass(paste0("Enter gSD password for user '", Sys.getenv("gSD_user"), "':")))
-Sys.setenv(gSD_downtests = FALSE)
+Sys.setenv(gSD_downtests = "no")
 
-# recreate product list
-source("dev/update_product_list.R")
+Sys.setenv(gSD_updprod = "no")
 
 # run tests
 devtools::test()
--- tests/testthat/helper-vars.R
+++ tests/testthat/helper-vars.R
@@ -6,8 +6,26 @@ do <- list(
   downloads = if(Sys.getenv("gSD_downtests") == "yes") TRUE else FALSE
 )
 
+# logins
+if(Sys.getenv("gSD_user") != ""){
+  auth <- TRUE
+  try(login_CopHub(Sys.getenv("gSD_user"), Sys.getenv("gSD_pass")))
+  try(login_USGS(Sys.getenv("gSD_user"), Sys.getenv("gSD_pass")))
+}else{
+  auth <- FALSE
+}
+
+# update offline product list?
+if(all(Sys.getenv("gSD_updprod") == "yes", auth)){
+  .prod.list <- get_products(update_online = T, grouped = T)
+  usethis::use_data(.prod.list, internal = TRUE, overwrite = T)
+  message("Build and install getSpatialData ater tests finished to include updated product list in future runs.")
+  products <- .prod.list
+} else{
+  products <- try(get_products(grouped = T, update_online = F))
+}
+
 # vars
-products <- try(get_products(grouped = T, update_online = F))
 vars <- list(
   dir.arc = tempdir(),
   prods =  do.call(rbind, lapply(names(products), function(group){
@@ -20,13 +38,3 @@ vars <- list(
 vars$prods$aoi <- list(aoi_data[[1]])
 vars$prods$time_range <- list(c("2019-03-01", "2019-03-30"))
 vars$prods$time_range[grepl("landsat_tm", vars$prods$product) | grepl("landsat_etm", vars$prods$product) | grepl("landsat_mss", vars$prods$product)] <- list(c("2010-03-01", "2010-03-30"))
-
-
-# logins
-if(Sys.getenv("gSD_user") != ""){
-  auth <- TRUE
-  try(login_CopHub(Sys.getenv("gSD_user"), Sys.getenv("gSD_pass")))
-  try(login_USGS(Sys.getenv("gSD_user"), Sys.getenv("gSD_pass")))
-}else{
-  auth <- FALSE
-}
